57c1dab0ea6cf03baaf96e547aa728b3ccac6527
Fakhir Ahmed Khan <fakhirahmed898@gmail.com>
2025-06-05 11:56:57 +0400

    feat: enhance cart item deletion by targeting specific item containers in checkout

--- Script/checkout.js
+++ Script/checkout.js
@@ -11,7 +11,8 @@ cart.forEach((cartItem) => {
   if (matchingProduct) {
     cartSummaryHTML +=
       `
-      <div class="cart-item-container">
+      <div class="cart-item-container 
+      js-cart-item-container${matchingProduct.id}">
         <div class="delivery-date">Delivery date: Tuesday, June 21</div>
 
         <div class="cart-item-details-grid">
@@ -70,6 +71,10 @@ document.querySelectorAll('.js-delete-btn')
     btn.addEventListener('click', () => {
       const productId = btn.dataset.productId;
       removeFromCart(productId);
-      console.log(cart);
+
+      const container = document.querySelector(
+        `.js-cart-item-container${productId}`
+      )
+      container.remove();
     });
   });
