1dac404f65fd7bfe932b0ef0b08b5f45685197bd
Fakhir Ahmed Khan <fakhirahmed898@gmail.com>
2025-06-07 20:20:33 +0400

    feat: add delivery options functionality to cart and checkout

---
+++ data/deliveryOptions.js
@@ -0,0 +1,13 @@
+export const deliveryOptions = [{
+    id: '1',
+    deliveryDays: 7, // Fixed property name
+    priceCents: 0,
+}, {
+    id: '2',
+    deliveryDays: 3, // Fixed property name
+    priceCents: 499,
+}, {
+    id: '3',
+    deliveryDays: 1, // Fixed property name
+    priceCents: 999,
+}];
